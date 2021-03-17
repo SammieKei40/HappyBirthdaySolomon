156eedf61a3dd438ff18b151071b97186bb34c82
Sammie <sammiekei9@gmail.com>
2021-03-17 09:38:29 +0100

    Edited all files

--- js.js
+++ js.js
@@ -9,27 +9,42 @@ const colors = [
 
 function createSquare(){
     const section = document.querySelector('section');
+    const main = document.querySelector('main');
     const square = document.createElement('span');
+    const line = document.createElement('main');
+
 
     var size = Math.random() * 50;
 
     square.style.width = 20 + size +'px';
-    square.style.height = 20 + size + 'px'; 
+    square.style.height = 20 + size + 'px';
 
     square.style.top = Math.random() * innerHeight + 'px';
     square.style.left = Math.random() * innerWidth + 'px';
 
+    
+    line.style.width = 2 + size +'px';
+    line.style.height = 10 + size + 'px';
+
+    line.style.top = Math.random() * innerHeight + 'px';
+    line.style.left = Math.random() * innerWidth + 'px';
+
+
+  
     const bg = colors[Math.floor(Math.random() * colors.length)];
     
     square.style.background = bg;
+    line.style.background = bg;
 
     section.appendChild(square);
+    main.appendChild(line);
 
     
     
 
     setTimeout(() => {
         square.remove()
+        line.remove()
     },20000)
 }
 
